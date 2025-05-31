0a588879b324d6b7924c523e325f2ecd2c22cd9d
harsyax1 <113166639+bubuuble@users.noreply.github.com>
2025-06-01 00:20:16 +0700

    update speed insight and analytics

--- app/(main)/layout.tsx
+++ app/(main)/layout.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import Navbar from '../components/navbar';
 import Footer from '../components/footer';
+import { Analytics } from '@vercel/analytics/next';
+import { SpeedInsights } from '@vercel/speed-insights/next';
 
 export const metadata = {
   icons: {
@@ -18,6 +20,8 @@ export default function MainLayout({
       <Navbar />
       <main className="flex-grow">
         {children}
+        <Analytics />
+        <SpeedInsights />
       </main>
       <Footer />
     </div>
